81ec01cbbddf2419de99cca2ac96668dbf8e6be7
Danny Engelman <danny@engelman.nl>
2021-05-24 20:14:01 +0200

    V1

--- elements.uri.js
+++ elements.uri.js
@@ -1,146 +1,163 @@
-let D = document.currentScript.src.split`-`;
-let e =
-  "-customElements-define-HTMLElement-attachShadow-innerHTML-nodeName-querySelector-input-split-render";
-e = "-customElements-define-HTMLElement-attachShadow";
-class t extends window[D[3]] {
+//let URI = "";
+let URI = "https://raw.githubusercontent.com/urifier/urifier.github.io/main/elements.min.js";
+let URIStrings = "-customElements-define-HTMLElement-attachShadow-innerHTML-nodeName-querySelector-input-split-render";
+//URIStrings = "-customElements-define-HTMLElement-attachShadow";
+
+function minify(js='Textual content') {
+  (async () => {
+    const rawResponse = await fetch('https://javascript-minifier.com/raw', {
+      method: 'POST',
+       mode: 'no-cors', 
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        input: js
+      })
+    });
+    const content = await rawResponse.json();
+    console.log(666,content);
+  })();
+}
+//minify("let aap=21;");
+class URIElement extends window.HTMLElement {
   constructor() {
-    super()[D[4]]({ mode: "open" })[D[5]] = "";
+    super().attachShadow({
+      mode: 'open'
+    }).innerHTML = ``;
   }
-  title(e = this[D[6]]) {
-    return `<h4 style="margin:5px 0">${e}</h4>`;
+  title(txt = this.nodeName) {
+    return `<h4 style="margin:5px 0">${txt}</h4>`;
   }
   get URI() {
-    return document[D[7]]("uri-input#URI").query("input").value;
+    return document.querySelector("uri-input#URI").query("input").value;
   }
   get DictVar() {
-    return document[D[7]]("uri-input#VAR").query("input").value;
+    return document.querySelector("uri-input#VAR").query("input").value;
   }
   get source() {
-    let e = document[D[7]]("uri-source");
-    return e && e.query ? e.query("textarea")[D[5]] : "";
+    let sourceElement = document.querySelector("uri-source");
+    if (sourceElement && sourceElement.query){
+      return sourceElement.query("textarea").innerHTML;
+    }else
+      return "";
   }
   dictionary() {
-    return this.URI[D[9]]("-");
+    return this.URI.split("-");
   }
   connectedCallback() {
-    this[D[10]]();
+    this.render();
     this.connected && this.connected();
-    this.URIfier.addEventListener("update", (e) => {
+    this.URIfier.addEventListener("update", (evt) => {
       this.update && this.update();
     });
   }
-  log(...e) {
-    document.body.append(...e, document.createElement("br"));
+  log(...txt) {
+    document.body.append(...txt, document.createElement("br"));
   }
-  query(e, t = this.shadowRoot || this) {
-    return t[D[7]](e);
+  query(selector, root = this.shadowRoot || this) {
+    return root.querySelector(selector);
   }
   get URIfier() {
-    return document[D[7]]("URIfier");
+    return document.querySelector("URIfier");
   }
   dispatch({
-    name: e = this[D[6]],
-    detail: t = {},
-    options: i = { detail: t, bubbles: !0, composed: !0, cancelable: !1 },
-    root: s = this,
+    name = this.nodeName,
+    detail = {},
+    options = {
+      detail,
+      bubbles: true,
+      composed: true,
+      cancelable: false,
+    },
+    root = this
   }) {
-    s.dispatchEvent(new CustomEvent(e, i));
+    root.dispatchEvent(new CustomEvent(name, options));
   }
-  dispatchUpdate(e = {}) {
-    this.dispatch({ name: "update", detail: e });
+  dispatchUpdate(detail = {}) {
+    this.dispatch({
+      name: "update",
+      detail
+    });
   }
-  set HTML(e) {
-    this.shadowRoot[D[5]] = e;
+  set HTML(html) {
+    this.shadowRoot.innerHTML = html;
   }
 }
-class i extends t {
+class URITextarea extends URIElement {
   render() {
     fetch(this.URI)
-      .then((e) => e.text())
-      .then((e) => {
-        this.setText(e);
+      .then(res => res.text())
+      .then(txt => {
+        console.warn("%c fetched ", "background:gold", txt.length, "Bytes", this.URI);
+        this.setText(txt)
         this.dispatchUpdate();
       });
   }
-  textarea(e, t = e.length) {
-    e = e.replace(/</g, "<");
-    this.HTML = `<style>textarea{width:100%;height:10em}</style>${this.title(
-      this[D[6]] + ` ${t} Bytes`
-    )}<textarea>${e}</textarea>`;
+  textarea(txt, size = txt.length) {
+    txt = txt.replace(/</g, "&lt;");
+    this.HTML = `<style>textarea{width:100%;height:10em}</style>${this.title(this.nodeName+` ${size} Bytes`)}<textarea>${txt}</textarea>`;
   }
 }
-window[D[1]][D[2]](
-  "uri-input",
-  class extends t {
-    render() {
-      let e = "D";
-      "URI" == this.id &&
-        (e =
-          localStorage.getItem("URIinput") ||
-          "https://raw.githubusercontent.com/urifier/urifier.github.io/main/elements.min.js?-customElements-define-HTMLElement-attachShadow");
-      this.HTML = this.title() + `<input style="width:100%" value="${e}">`;
-    }
-    connected() {
-      this.query("input").onkeyup = (e) => {
-        this.dispatchUpdate(e.target.value);
-      };
-    }
+window.customElements.define('uri-input', class extends URIElement {
+  render() {
+    let value = "D";
+    if (this.id == "URI") value = localStorage.getItem("URIinput") || URI + "?" + URIStrings;
+    this.HTML = this.title(this.getAttribute("title")) + `<input style="width:100%" value="${value}">`;
   }
-);
-window[D[1]][D[2]](
-  "uri-string",
-  class extends t {
-    render() {
-      let e = "." + this.getAttribute("string"),
-        t = this.source[D[9]](new RegExp(e)).length;
-      this.HTML = `<div>${this.getAttribute("idx")}. <b>${t}</b> ${e}</div>`;
-    }
-    update() {
-      this[D[10]]();
-    }
+  connected() {
+    this.query("input").onkeyup = (evt) => {
+      this.dispatchUpdate(evt.target.value);
+    };
   }
-);
-window[D[1]][D[2]](
-  "uri-dictionary",
-  class extends t {
-    render() {
-      this.URIfier.strings = {};
-      let e = this.dictionary();
-      this.HTML =
-        this.title() +
-        e.map((e, t) => {
-          t && (this.URIfier.strings[e] = { idx: t, count: 0 });
-          return `<uri-string idx="${t}" string="${e}"></uri-string>`;
-        }).join``;
-    }
-    update() {
-      this[D[10]]();
-    }
+});
+window.customElements.define('uri-string', class extends URIElement {
+  render() {
+    let str = "." + this.getAttribute("string");
+    let count = this.source.split(new RegExp(str)).length;
+    this.HTML = `<div>${this.getAttribute("idx")}. <b>${count}</b> ${str}</div>`;
   }
-);
-window[D[1]][D[2]](
-  "uri-source",
-  class extends i {
-    setText(e) {
-      this.textarea(e);
-    }
+  update() {
+    this.render();
   }
-);
-window[D[1]][D[2]](
-  "uri-compressed",
-  class extends i {
-    setText(e) {
-      let t = "let " + this.DictVar + "=document.currentScript.src[D[9]]`-`;";
-      this.textarea(t + e);
-    }
-    update() {
-      let e = this.source;
-      Object.keys(this.URIfier.strings).map((t) => {
-        let i = this.URIfier.strings[t];
-        t = "." + t;
-        e = e.replaceAll(t, `[${this.DictVar}[${i.idx}]]`);
-      });
-      this.setText(e);
-    }
+});
+window.customElements.define('uri-dictionary', class extends URIElement {
+  render() {
+    this.URIfier.strings = {};
+    let dict = this.dictionary();
+    this.HTML = this.title() +
+      dict.map((str, idx) => {
+        if (idx) this.URIfier.strings[str] = {
+          idx,
+          count: 0
+        };
+        //          let count = txt.split(str);
+        return `<uri-string idx="${idx}" string="${str}"></uri-string>`
+      }).join ``;
   }
-);
+  update() {
+    this.render();
+  }
+});
+window.customElements.define('uri-source', class extends URITextarea {
+  setText(txt) {
+    this.textarea(txt);
+  }
+});
+window.customElements.define('uri-compressed', class extends URITextarea {
+  setText(txt) {
+    let prefix = "let " + this.DictVar + "=document.currentScript.src.split`-`;";
+    //prefix += `let D="X-${this.dictionary().slice(1).join("-")}".split("-");`;
+    this.textarea(prefix + txt);
+  }
+  update() {
+    let txt = this.source;
+    Object.keys(this.URIfier.strings).map(str => {
+      let data = this.URIfier.strings[str];
+      str = "." + str;
+      txt = txt.replaceAll(str, `[${this.DictVar}[${data.idx}]]`)
+    });
+    this.setText(txt);
+  }
+});
